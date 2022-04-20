9ad6a278ca758f09e008cff9b38480c409b7eaab
darwinQSot <quispesotodaniel@gmail.com>
2022-04-19 23:41:12 -0500

    Sesion 5 - Resilience4J : Personalizando parámetro del circuit Breaker

--- items/src/main/java/com/dardan/springboot/app/item/AppConfig.java
+++ items/src/main/java/com/dardan/springboot/app/item/AppConfig.java
@@ -1,9 +1,15 @@
 package com.dardan.springboot.app.item;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.beans.Customizer;
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
@@ -12,5 +18,18 @@ public class AppConfig {
 	@LoadBalanced
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id -> {
+			return new Resilience4JConfigBuilder(id)
+					.circuitBreakerConfig()(CircuitBreakerConfig.custom()
+					.slidingWindowSize(10)
+					.failureRateThreshold(50)
+					.waitDurationInOpenState(Duration.ofSeconds(10L))
+					.build())
+
+		});
 	}
 }
