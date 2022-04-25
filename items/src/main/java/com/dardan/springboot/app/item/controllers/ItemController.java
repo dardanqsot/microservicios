6dcd233233fcb2fafd2e23620030677b8d798f7d
darwinQSot <quispesotodaniel@gmail.com>
2022-04-24 23:12:57 -0500

    Sesion 5 - Resilience4J: Anotaciones @CircuitBreaker y @TimeLimiter

--- items/src/main/java/com/dardan/springboot/app/item/controllers/ItemController.java
+++ items/src/main/java/com/dardan/springboot/app/item/controllers/ItemController.java
@@ -1,7 +1,10 @@
 package com.dardan.springboot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +47,19 @@ public class ItemController {
 				.run(()-> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad, e));
 	}
 
+	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
+	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
+	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+
+	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name="items")
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return CompletableFuture.supplyAsync(()-> itemService.findById(id, cantidad));
+	}
+
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
 		logger.info(e.getMessage());
 		Item item = new Item();
@@ -55,6 +71,19 @@ public class ItemController {
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return item;
+	}
+
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return CompletableFuture.supplyAsync(()-> item);
 	}
 
 }
