2cd207b6efa44fbc6069886ccdb706c2da861e48
darwinQSot <quispesotodaniel@gmail.com>
2022-05-08 19:21:12 -0500

    Sesion 6: Spring Cloud Config Server: centralizando la configuración - Creacción de proyecto

---
+++ config-server/src/test/java/com/dardan/springboot/app/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dardan.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
