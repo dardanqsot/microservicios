d0ba585ec56dcb39f5cb780846fc6f359e4e0c52
darwinQSot <quispesotodaniel@gmail.com>
2022-05-22 21:48:43 -0500

    Sesion 6: Spring Cloud Config Server: configurando profiles en servicio items

--- items/src/main/java/com/dardan/springboot/app/item/controllers/ItemController.java
+++ items/src/main/java/com/dardan/springboot/app/item/controllers/ItemController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,9 @@ import com.dardan.springboot.app.item.models.service.ItemService;
 public class ItemController {
 
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
@@ -100,6 +104,12 @@ public class ItemController {
 		Map<String, String> json = new HashMap<>();
 		json.put("texto", texto);
 		json.put("puerto", puerto);
+
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals(("dev"))){
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
